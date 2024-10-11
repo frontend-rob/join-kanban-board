9d04c34ecc560fcc6a4300999805e734abfb6606
Steven <steven.rudko@outlook.com>
2024-10-11 20:05:20 +0200

    board structure created

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-fetch("./templates/sidebar.html")
-  .then((response) => response.text())
-  .then((data) => {
-    document.querySelector("nav").innerHTML = data;
-  });
-
-fetch("./templates/header.html")
-  .then((response) => response.text())
-  .then((data) => {
-    document.querySelector("header").innerHTML = data;
-  });
+async function includeHTML() {
+  let includeElements = document.querySelectorAll('[include-html]');
+  for (let i = 0; i < includeElements.length; i++) {
+      const element = includeElements[i];
+      const file = element.getAttribute("include-html");
+      let resp = await fetch(file);
+      if (resp.ok) {
+          element.innerHTML = await resp.text();
+      } else {
+          element.innerHTML = 'page not found';
+      }
+  }
+}
