da786d01f3d3facef9e6d96a387c56885de094c8
R.G. <frontend-rob@proton.me>
2024-11-03 10:11:53 +0100

    updated template.js
    this update includes:
    
    - implementation of raw svg icons in header and sidebar navigation,
    
    - fixed the respective css files
    
    - optimized accessibility by adding aria-labels on buttons

--- js/templates.js
+++ js/templates.js
@@ -1,14 +1,18 @@
 function getHeaderContent() {
     return `
         <a href="../pages/summary.html" class="logo-container">
-            <img class="logo-header" src="../assets/img/join-logo-dark.svg" alt="Join Logo" />
+            <img class="logo-header" src="../assets/img/join-logo-dark.svg" alt="Join Logo"/>
         </a>
         <span class="project-name">Kanban Project Managment Tool</span>
         <div id="header-menu" class="header-menu">
-            <a class="help-icon" href="../pages/help.html">
-                <img src="../assets/icons/help.svg" alt="help" />
+            <a href="../pages/help.html" class="help-icon" aria-label="Read more about Join">
+                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="none" viewBox="0 0 256 256">
+                    <path
+                        d="M140,180a12,12,0,1,1-12-12A12,12,0,0,1,140,180ZM128,72c-22.06,0-40,16.15-40,36v4a8,8,0,0,0,16,0v-4c0-11,10.77-20,24-20s24,9,24,20-10.77,20-24,20a8,8,0,0,0-8,8v8a8,8,0,0,0,16,0v-.72c18.24-3.35,32-17.9,32-35.28C168,88.15,150.06,72,128,72Zm104,56A104,104,0,1,1,128,24,104.11,104.11,0,0,1,232,128Zm-16,0a88,88,0,1,0-88,88A88.1,88.1,0,0,0,216,128Z">
+                    </path>
+                </svg>
             </a>
-            <button id="menu-toggle-button" class="profile-button" onclick="toggleMobileMenu()">
+            <button id="menu-toggle-button" class="profile-button" aria-label="help-btn" onclick="toggleMobileMenu()">
                 <span id="current-user-initials" class="profile-initials">SM</span>
             </button>
             <div id="mobile-menu" class="mobile-menu hidden">
@@ -23,41 +27,57 @@ function getHeaderContent() {
 
 function getNavigationContent() {
     return `
-        <nav role="navigation">
-            <a href="../pages/summary.html" class="nav-logo">
-                <img src="../assets/img/join-logo.svg" alt="Join Logo" />
-            </a>
-            <div id="main-menu" class="menu">
-                <a href="../pages/summary.html" class="nav-link">
-                    <div class="menu-item">
-                        <img class="nav-icon" src="../assets/icons/layout.svg" alt="Summary Icon" />
-                        Summary
-                    </div>
-                </a>
-                <a href="../pages/add-task.html" class="nav-link">
-                    <div class="menu-item">
-                        <img class="nav-icon" src="../assets/icons/note-pencil.svg" alt="Add Task Icon" />
-                        Add Task
-                    </div>
-                </a>
-                <a href="../pages/board.html" class="nav-link">
-                    <div class="menu-item">
-                        <img class="nav-icon" src="../assets/icons/kanban.svg" alt="Board Icon" />
-                        Board
-                    </div>
-                </a>
-                <a href="../pages/contacts.html" class="nav-link">
-                    <div class="menu-item">
-                        <img class="nav-icon" src="../assets/icons/address-book.svg" alt="Contacts Icon" />
-                        Contacts
-                    </div>
-                </a>
+<nav role="navigation">
+    <a href="../pages/summary.html" class="nav-logo">
+        <img src="../assets/img/join-logo.svg" alt="Join Logo"/>
+    </a>
+    <div id="main-menu" class="menu">
+        <a href="../pages/summary.html" class="nav-link">
+            <div class="menu-item">
+                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="none" viewBox="0 0 256 256">
+                    <path
+                        d="M216,40H40A16,16,0,0,0,24,56V200a16,16,0,0,0,16,16H216a16,16,0,0,0,16-16V56A16,16,0,0,0,216,40Zm0,16V96H40V56ZM40,112H96v88H40Zm176,88H112V112H216v88Z">
+                    </path>
+                </svg>
+                Summary
             </div>
-            <div class="footer-links">
-                <a href="../pages/privacy-policy.html" class="footer-link">Privacy Policy</a>
-                <a href="../pages/legal-notice.html" class="footer-link">Legal Notice</a>
+        </a>
+        <a href="../pages/add-task.html" class="nav-link">
+            <div class="menu-item">
+                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="none" viewBox="0 0 256 256">
+                    <path
+                        d="M229.66,58.34l-32-32a8,8,0,0,0-11.32,0l-96,96A8,8,0,0,0,88,128v32a8,8,0,0,0,8,8h32a8,8,0,0,0,5.66-2.34l96-96A8,8,0,0,0,229.66,58.34ZM124.69,152H104V131.31l64-64L188.69,88ZM200,76.69,179.31,56,192,43.31,212.69,64ZM224,128v80a16,16,0,0,1-16,16H48a16,16,0,0,1-16-16V48A16,16,0,0,1,48,32h80a8,8,0,0,1,0,16H48V208H208V128a8,8,0,0,1,16,0Z">
+                    </path>
+                </svg>
+                Add Task
             </div>
-        </nav>
+        </a>
+        <a href="../pages/board.html" class="nav-link">
+            <div class="menu-item">
+                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="none" viewBox="0 0 256 256">
+                    <path
+                        d="M216,48H40a8,8,0,0,0-8,8V208a16,16,0,0,0,16,16H88a16,16,0,0,0,16-16V160h48v16a16,16,0,0,0,16,16h40a16,16,0,0,0,16-16V56A8,8,0,0,0,216,48ZM88,208H48V128H88Zm0-96H48V64H88Zm64,32H104V64h48Zm56,32H168V128h40Zm0-64H168V64h40Z">
+                    </path>
+                </svg>
+                Board
+            </div>
+        </a>
+        <a href="../pages/contacts.html" class="nav-link">
+            <div class="menu-item">
+                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="none" viewBox="0 0 256 256">
+                    <path
+                        d="M83.19,174.4a8,8,0,0,0,11.21-1.6,52,52,0,0,1,83.2,0,8,8,0,1,0,12.8-9.6A67.88,67.88,0,0,0,163,141.51a40,40,0,1,0-53.94,0A67.88,67.88,0,0,0,81.6,163.2,8,8,0,0,0,83.19,174.4ZM112,112a24,24,0,1,1,24,24A24,24,0,0,1,112,112Zm96-88H64A16,16,0,0,0,48,40V64H32a8,8,0,0,0,0,16H48v40H32a8,8,0,0,0,0,16H48v40H32a8,8,0,0,0,0,16H48v24a16,16,0,0,0,16,16H208a16,16,0,0,0,16-16V40A16,16,0,0,0,208,24Zm0,192H64V40H208Z">
+                    </path>
+                </svg>
+                Contacts
+            </div>
+        </a>
+    </div>
+    <div class="footer-links">
+        <a href="../pages/privacy-policy.html" class="footer-link">Privacy Policy</a>
+        <a href="../pages/legal-notice.html" class="footer-link">Legal Notice</a>
+    </div>
+</nav>
     `;
 }
 
